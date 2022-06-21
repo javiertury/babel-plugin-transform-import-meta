f377c6552c338443cd3560dc76d50e893f0e9682
javiertury <javiertury@gmail.com>
2022-06-21 19:54:11 +0200

    chore: replace strip-indent with ts-dedent

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,14 @@
 import * as babelCore from '@babel/core';
-import stripIndent from 'strip-indent';
+import dedent from 'ts-dedent';
 import importMetaPlugin from './index';
 
 describe('babel-plugin-import-meta', () => {
   test('transforms import.meta.url', () => {
-    const input = stripIndent(`
+    const input = dedent(`
       console.log(import.meta.url);
     `);
 
-    const expected = stripIndent(`
+    const expected = dedent(`
       console.log(require('url').pathToFileURL(__filename).toString());
     `);
     const result = babelCore.transform(input, {
@@ -18,11 +18,11 @@ describe('babel-plugin-import-meta', () => {
   });
 
   test('does not transform non-meta property', () => {
-    const input = stripIndent(`
+    const input = dedent(`
       console.log(foo.import.meta);
     `);
 
-    const expected = stripIndent(`
+    const expected = dedent(`
       console.log(foo.import.meta);
     `);
     const result = babelCore.transform(input, {
@@ -32,11 +32,11 @@ describe('babel-plugin-import-meta', () => {
   });
 
   test('does not transform import.meta if known property is not specified', () => {
-    const input = stripIndent(`
+    const input = dedent(`
       console.log(import.meta);
     `);
 
-    const expected = stripIndent(`
+    const expected = dedent(`
       console.log(import.meta);
     `);
     const result = babelCore.transform(input, {
@@ -46,11 +46,11 @@ describe('babel-plugin-import-meta', () => {
   });
 
   test('does not transform unknown meta properties', () => {
-    const input = stripIndent(`
+    const input = dedent(`
       console.log(import.meta.foo);
     `);
 
-    const expected = stripIndent(`
+    const expected = dedent(`
       console.log(import.meta.foo);
     `);
     const result = babelCore.transform(input, {
